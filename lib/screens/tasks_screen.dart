f1431bd625baa3c0822ecbed5437471f6a42d7b0
Fiyin Kolawole <kolawolemmanuel@hotmail.com>
2020-01-08 14:51:10 +0100

    Create widgets for displaying tasks

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
@@ -45,6 +46,7 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -52,6 +54,7 @@ class TasksScreen extends StatelessWidget {
                   topRight: Radius.circular(30.0),
                 ),
               ),
+              child: TasksList(),
             ),
           ),
         ],
