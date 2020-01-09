d3ecd54fdf63459972ce43852fa7c160d54183ee
Fiyin Kolawole <kolawolemmanuel@hotmail.com>
2020-01-09 10:10:05 +0100

    Add local state to tasks list

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void onChanged(newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('My task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        'My task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(isChecked: isChecked, onChanged: onChanged),
     );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({
+    @required this.isChecked,
+    @required this.onChanged,
+  });
+
+  final bool isChecked;
+  final Function onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: isChecked, onChanged: onChanged);
   }
 }
